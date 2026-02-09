184e47642d826c58750d644893aae2b035e363d5
Roopa-kr <nisharoopa3@gmail.com>
2026-02-09 10:42:26 +0530

    Deque Operations

--- dequeue.py
+++ dequeue.py
@@ -3,3 +3,9 @@ stack=deque()
 for i in range(0,10):
     stack.append(i)
 print(stack)
+#To remove the last element
+print(stack.pop())
+print(stack)
+#To remove the first element
+print(stack.popleft())
+print(stack)
