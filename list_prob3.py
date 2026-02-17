2d37a4f1b059de13398d3a1a6d5dfef88bf3e033
Roopa-kr <nisharoopa3@gmail.com>
2026-02-17 21:58:21 +0530

    Expected output given

--- list_prob3.py
+++ list_prob3.py
@@ -1,8 +1,9 @@
 # Given a list of integers, create a new list
-#expected output: [1, 20, 11, 4, 9]
-l=[1,20,11,4,78,9,0,20]
-lst=[]
-for i in l[:4]:
-    lst.append(i)
-lst.append(l[5])
-print(lst)
+#expected output: [1, 20, 11, 9]
+l=[1,20,11,4,78,9,0,20] #impppppppppppppp create new list from the existing
+ex = [78,0,4]
+res = []
+for i in l:
+    if i not in ex and i not in res:
+        res.append(i)
+print(res)
