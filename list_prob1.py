709568a92a87b6366afb9b95338ef1cd893d6664
Roopa-kr <nisharoopa3@gmail.com>
2026-02-17 19:56:39 +0530

    Expected output given

---
+++ list_prob1.py
@@ -0,0 +1,9 @@
+# Given a list of integers, create a new list 
+# where each element is double the value of list    
+#expected output: [2, 4, 6, 8, 10]
+lst=[1,2,3,4,5,6,7,8,9,10]
+lst1=[]
+for i in lst:
+    if i%2==0:
+        lst1.append(i)
+print(lst1)
