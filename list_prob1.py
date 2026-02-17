80024b879c153d8fafee72050cf34d5115f17595
Roopa-kr <nisharoopa3@gmail.com>
2026-02-17 19:58:44 +0530

    Expected output given

--- list_prob1.py
+++ list_prob1.py
@@ -1,5 +1,5 @@
 # Given a list of integers, create a new list 
-# where each element is double the value of list    
+# where each element is even the value of list    
 #expected output: [2, 4, 6, 8, 10]
 lst=[1,2,3,4,5,6,7,8,9,10]
 lst1=[]
